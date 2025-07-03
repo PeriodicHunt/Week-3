cb87997125ac47b6468d32e16000458b98dddeb6
PeriodicHunt <mavila8862@gmail.com>
2025-07-03 13:37:30 -0700

    main function

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ def main():
   print("Welcome to the Calculator!")
 
   #TODO: get user input (float) for two numbers
+  a = float(input("Enter the first number: "))
+  b = float(input("Enter the second number: "))
   
   #TODO: ask user for operation
+  op = input("Choose operation (+ - * /): ")
 
   #TODO: use if statements to perform the correct operation
   # if op == '+':
@@ -15,8 +18,20 @@ def main():
   #elif op == '-':
     #result = sub(a, b)
   #...
+  if op == "+": 
+     result = add(a, b)
+  elif op == "-" :
+     result = sub(a, b)
+  elif op == "*" :
+     result = mul(a, b)
+  elif op == "/" :
+     result = div(a, b)
+  else:
+     print("Please try again and enter a valid operation.")
+     return
 
   #print the result 
+  print("Result: " + str(result))
   #print("Result: " + result)
 
 #running program 
